7e88082349a48b6029a805d4b224850f4e08e1c7
Carlos Perry <carlos.perry@hakro.com>
2022-02-22 14:15:39 +0100

    added xlsx

--- appdata/app.py
+++ appdata/app.py
@@ -133,7 +133,16 @@ def summary():
     
     logo_path = os.path.join(app.config['UPLOAD_FOLDER'], session_id, s1['file_logo'])
     data_path = os.path.join(app.config['UPLOAD_FOLDER'], session_id, s1['file_data'])
-    df = pd.read_csv(data_path, header=None, sep=';')
+    if s1['file_data'].endswith('.csv') or s1['file_data'].endswith('.CSV') :
+            df = pd.read_csv(data_path, header=None, sep=';')
+    elif s1['file_data'].endswith('.xlsx') or s1['file_data'].endswith('.XLSX'):
+            df = pd.read_excel(data_path, header=None)
+    elif s1['file_data'].endswith('.xls') or s1['file_data'].endswith('.XLS'):
+            pass
+    else:
+        return redirect(url_for('error', e=200))
+
+
     len_df = len(df)
     if request.method == 'POST':
         s1['opt1'] = datetime.now()
